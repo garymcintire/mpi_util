651cd242198e8548e013ab4da77c83c7ce3ae20d
garymcintire <3gmcintire@gmail.com>
2017-09-05 11:13:04 -0500

    fixed cache

--- mpi_util.py
+++ mpi_util.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import os, subprocess, sys
 import numpy as np
 import random
+import time
 
 """
 
@@ -25,7 +26,7 @@ import random
 	import mpi_util
 
 3. When nprocs is known shortly after program start, fork nprocs...
-	if "parent" == mpi_util.mpi_fork(args.nprocs): os.exit()
+	if "parent" == mpi_util.mpi_fork(args.nprocs): sys.exit()
 
 4. Each process and environment will need random different seeds computed from
     mpi_util.set_global_seeds(seed+mpi_util.rank)
@@ -48,29 +49,61 @@ import random
     mpi_util.rank0_bcast_wts(val_func.sess, val_func.g, 'val')
     mpi_util.rank0_bcast_wts(policy.sess, policy.g, 'policy')
 
+9.  You should be able to use multiple gpu cards if you have them (untested))
+    with tf.device('/gpu:'+mpi_util.rank%2):   
+        main()
+
+10. You should be able to use multiple computers as well. See mpirun documentation on host files
+
 
     This code is for tensorflow, but a few alterations would allow it to work on theano, pytorch, etc
 
     The actual speedup from parallelizing is most dependent on the batch size. Larger batch sizes will get a closer to
-    linear speedup, but even small batche sizes can triple or quadruple your wall clock speed with nprocs = 10
+    linear speedup, but even small batche sizes can triple or quadruple your wall clock speed with nprocs = 8
 
+
+                        Timing
+    python ../train.py Walker2d-v1 --nprocs 10 --gpu_pct 0.05  -n 2000
+
+nprocs  steps_per_sec   reward_mean
+1            546             641        # reward is highly variable because robot is highly variable
+2           1040             544
+3           1712             611
+4           2014            1220
+5           1950            1513
+6           2100             860
+7           2300             681
+8           2400             452
+9           2484             359
+10          2339             367
+11          2384             410
+12          2169             336
 """
 
 
-batches_or_update = 1   # This is just a flag to indicate we are accumulating batches.  accum batches or update process wts in parallel
-
-tf_config = tf.ConfigProto(
-    gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=.05, allow_growth=True),
-    inter_op_parallelism_threads=1,
-    intra_op_parallelism_threads=1)
+tf_config = None
 
 nworkers=1      # defaults for a single process
 rank= 0
 
-def mpi_fork(n):
+def mpi_fork(n, gpu_pct=0.0):
     """Re-launches the current script with workers
     Returns "parent" for original parent, "child" for MPI children
     """
+    global tf_config
+    if gpu_pct > 0:
+        tf_config = tf.ConfigProto(
+            gpu_options =tf.GPUOptions(
+                                per_process_gpu_memory_fraction=gpu_pct,
+                                # allow_growth=True
+                                ),
+            inter_op_parallelism_threads=1,
+            intra_op_parallelism_threads=1)
+    else:
+        tf_config = tf.ConfigProto(
+            gpu_options =tf.GPUOptions( allow_growth=True),
+            inter_op_parallelism_threads=1,
+            intra_op_parallelism_threads=1)
     if n<=1:
         return "child"
     if os.getenv("IN_MPI") is None:
@@ -81,7 +114,8 @@ def mpi_fork(n):
             IN_MPI="1"
         )
         print( ["mpirun", "-np", str(n), sys.executable] + sys.argv)
-        subprocess.check_call(["mpirun", "-np", str(n), sys.executable] +['-u']+ sys.argv, env=env)
+        subprocess.check_call(["mpirun", "-np", str(n),   sys.executable] +['-u']+ sys.argv, env=env)    # this mpirun makes bcast take more time with each iteration
+        # subprocess.check_call(["/usr/bin/mpirun", "-np", str(n), '-mca', 'coll_tuned_bcast_algorithm', '0', sys.executable] +['-u']+ sys.argv, env=env)       # this mpirun is 1/3 the speed of the one above
         return "parent"
     else:
         global nworkers, rank
@@ -115,6 +149,21 @@ def accum_batches(segs):
     pass
     return combo
 
+def print0(*args, **kwargs):
+    if rank == 0:
+        print(*args, **kwargs)
+
+cumsum_steps=0
+tstart = time.time()
+def steps_sec(stepcnt):
+    global tstart, cumsum_steps
+    cumsum_steps += stepcnt
+    print0('cumsum_steps', cumsum_steps)   # only for step counting
+    etime = time.time()
+    # print0(str(rank)+'_steps_sec', stepcnt, 1000*(etime-tstart), stepcnt/(etime-tstart))
+    print0(str(rank)+'_steps_sec', stepcnt/(etime-tstart))
+    tstart = etime
+
 def oldrank0_accum_batches(segsin):
     segs = MPI.COMM_WORLD.gather(segsin)
     if rank != 0: return segsin
@@ -128,6 +177,7 @@ def mpitype(typ):
     else: return None
 
 def rank0_accum_batches(segins):
+    if nworkers == 1: return segins   # don't bother if only one process
     combo = dict()
     for k in sorted(segins):
         seq = segins[k]
@@ -152,7 +202,7 @@ def rank0_accum_batches(segins):
     else: return segins
 
 def all_accum_batches(segs):
-    segs = MPI.COMM_WORLD.Allgather(segs)
+    segs = MPI.COMM_WORLD.allgather(segs)
     return accum_batches(segs)
 
 def all_sum(x):
@@ -166,7 +216,35 @@ def all_mean(x):
     return all_sum(x) / nworkers
 
 def bcast(x):
-    return MPI.COMM_WORLD.bcast(x, root=0)
+    # print(str(rank)+'bcast sees',type(x),x.shape)
+    origshape = x.shape
+    if 1 or len(x.shape)==1:
+        # print(str(rank)+'if True')
+        MPI.COMM_WORLD.Bcast(x, root=0)
+    else:
+        print(str(rank)+'if False   prod',np.prod(x.shape))
+        inv = np.reshape(x, (np.prod(x.shape)))
+        print(str(rank)+'inv',type(inv),inv.shape,inv.dtype)
+        MPI.COMM_WORLD.Bcast(inv, root=0)
+        print(str(rank)+'outv',type(inv))
+        outv = inv.reshape(origshape)
+        print(str(rank)+'outv after reshape',type(outv),outv.shape)
+    return x
+def xxxbcast(x):
+    comm = MPI.COMM_WORLD
+    for i in range(1,nworkers):
+        if rank == 0:
+           comm.send(x, dest=i, tag=11)
+        elif rank == i:
+           x = comm.recv(source=0, tag=11)
+    return x
+def pt_to_pt_bcast(x):
+    for i in range(1,nworkers):
+        if rank == 0:
+            MPI.COMM_WORLD.Send(x, dest=i)
+        elif rank == i:
+            MPI.COMM_WORLD.Recv(x, source=0)
+    return x
 
 cache = {}
 def set_wt(sess, graph, var, val):
@@ -176,21 +254,25 @@ def set_wt(sess, graph, var, val):
         with graph.as_default():
             ph = tf.placeholder(val.dtype, shape=val.shape)
             op = var.assign(ph)
-            cache[k] = [ var, ph, op]
-    _, ph, op = cache[k]
+            cache[kc] = [ var, ph, op]
+    _, ph, op = cache[kc]
     sess.run(op, feed_dict={ph: val})
 
 def rank0_bcast_wts(sess, graph, label=''):
     if nworkers == 1: return    # don't bother if only one process
-    # for var in graph.get_collection('trainable_variables'):
     # chksum(sess, graph, label=label+'before'+str(rank))
     for var in sorted(graph.get_collection('trainable_variables'),key=lambda x: x.name):
-        # print('rank:',rank,'label',label,'var',var)
-        # tf.assign(var, all_mean(sess.run(var)))    suspected memory leaker
-        wts = bcast(sess.run(var))
+        # timeit(label+'______'+str(var))
+        w = sess.run(var)
+        # timeit(label+'sess.run'+str(var))
+        wts = bcast(w)
+        # print(str(rank)+'result type',type(wts))
+        # timeit(label+'bcast'+str(var))
         if rank != 0:
+            # print(label+'set_wts with type',type(wts), wts.shape, str(wts.__array_interface__['data']),var)
             set_wt(sess, graph, var, wts)
-            # sess.run(var, feed_dict={var: wts})
+            pass
+        # timeit(label+'set_wt'+str(var))
     # chksum(sess, graph, label=label+'after'+str(rank))
     # print('rank:',rank, 'nworkers',nworkers, 'label',label,'chksum', chksum)
 
@@ -203,7 +285,7 @@ def all_avg_wts(sess, graph, label=''):
         # print('rank:',rank,'label',label,'var',var)
         # tf.assign(var, all_mean(sess.run(var)))    suspected memory leaker
         wts = all_mean(sess.run(var))
-        chksum += wts.sum()
+        # chksum += wts.sum()
         set_wt(sess, graph, var, wts)
         # sess.run(var, feed_dict={var: wts})
     # print('rank:',rank, 'nworkers',nworkers, 'all_avg_wts label',label,'chksum', chksum)
@@ -220,5 +302,12 @@ def chksum(sess, graph, label=''):
         chksum += wts.sum()
         sess.run(var, feed_dict={var: wts})
     print('CHKSUM rank:',rank, 'nworkers',nworkers, 'label',label,'chksum', chksum)
+
+lasttime = 0.
+def timeit(label):
+    global lasttime
+    thistime = time.time()
+    if rank==0: print(str(rank)+'timed-'+label,thistime-lasttime)
+    lasttime = thistime
 
 
